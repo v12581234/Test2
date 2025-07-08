a96ab5b7d469abeaebb490e1f53732e718aa1c8c
v12581234 <vinhnguyen125812@gmail.com>
2025-07-08 13:03:48 +0700

    feature2 commit2

--- main.java
+++ main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
